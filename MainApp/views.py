c0d368b081a0fc808491d35c12b6a27731020132
BagasharZhumazhanov <bagashar.zhumazhanov@narxoz.kz>
2024-07-23 12:21:59 +0300

    Example 2

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'index.html')
+def index(request):    
+    products = [
+        {"id": 1, "name": "Adidas", "quantity": 5},
+        {"id": 2, "name": "Nike", "quantity": 3},
+        {"id": 3, "name": "Sprite", "quantity": 7},
+        {"id": 4, "name": "Coca-cola", "quantity": 4}
+    ]
+    return render(request, 'index.html', {'products': products})
 
+
+
+    
 
 
